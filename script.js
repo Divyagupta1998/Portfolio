f851f543d9f5c94ba6d747b53fa7be27d09703a0
Divya Gupta <divyagupta@host-190-218.iluruco.urbana.il.us.clients.pavlovmedia.net>
2025-01-09 05:19:49 -0600

    update

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add interactivity for button clicks or other events in the future
+document.querySelector('.hero button').addEventListener('click', () => {
+    alert('Welcome to Divya Gupta’s Portfolio!');
+});
